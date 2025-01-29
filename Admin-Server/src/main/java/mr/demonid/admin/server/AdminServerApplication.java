687e2be0da8e4fb2cfbf7f93280f914e23330fb8
ToaFromSiberia <143086731+ToaFromSiberia@users.noreply.github.com>
2025-01-30 02:18:43 +0300

    Add files via upload

---
+++ Admin-Server/src/main/java/mr/demonid/admin/server/AdminServerApplication.java
@@ -0,0 +1,15 @@
+package mr.demonid.admin.server;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+
+}
